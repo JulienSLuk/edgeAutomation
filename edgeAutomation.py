093e44e4bb08646e762ab04fe41fb7229dbfcec1
Julien Lukman <s10219568@connect.np.edu.sg>
2023-09-03 21:45:08 +0800

    csv setup

--- edgeAutomation.py
+++ edgeAutomation.py
@@ -2,15 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import concurrent.futures
-
-# Define your links
-links = ["https://example.com/page1", "https://example.com/page2", "https://example.com/page3"]
-username = "your_username"
-password = "your_password"
+import csv
 
 # Function to check if webpage is up after logging in
-def check_page(link):
-    driver = webdriver.Edge(executable_path='path_to_edge_driver')  # Specify the path to your Edge webdriver executable
+def check_page(link, username, password, driver_path):
+    driver = webdriver.Edge(executable_path=driver_path)
     driver.get(link)
     
     # Log in
@@ -31,9 +27,35 @@ def check_page(link):
     
     return link, page_title
 
+# Read data from CSV files
+links = []
+driver_paths = []
+usernames = []
+passwords = []
+
+with open('links.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        links.append(row[0])
+
+with open('driver_paths.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        driver_paths.append(row[0])
+
+with open('usernames.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        usernames.append(row[0])
+
+with open('passwords.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        passwords.append(row[0])
+
 # Check multiple links concurrently
 with concurrent.futures.ThreadPoolExecutor() as executor:
-    results = executor.map(check_page, links)
+    results = executor.map(check_page, links, usernames, passwords, driver_paths)
 
 # Print results
 for link, title in results:
